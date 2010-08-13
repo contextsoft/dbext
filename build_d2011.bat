805b18396a369b53bb80ca300c9e81c7b9e049d9
Mike <mike@contextsoft.com>
2010-08-13 18:03:30 +0300

    2011 support

---
+++ build_d2011.bat
@@ -0,0 +1,30 @@
+@echo off
+
+touch source\*.*
+
+cd packages
+
+SET package=dbExtPkg
+SET dpath=e:\programs\Embarcadero
+set include=e:\projects\Context\Common\source;
+
+set buildto=..\lib\d2011
+
+"%dpath%\RAD Studio\8.0\bin\dcc32.exe" -JL -I%include% -U%include% -LE%buildto% -LN%buildto% -N%buildto% -B %package%D2011.dpk
+
+move ..\source\*.hpp %buildto%
+move ..\source\*.obj %buildto%
+move ..\source\designtime\*.hpp %buildto%
+move ..\source\designtime\*.obj %buildto%
+move *.bpi %buildto%
+move *.bpl %buildto%
+move *.lib %buildto%
+move *.obj %buildto%
+move *.hpp %buildto%
+
+del %buildto%\CtxGridView.*
+del %buildto%\CtxPropView.*
+
+cd ..
+
+
